8725964239cccf1334b96cd420e299d265276132
Mahmoud Ali <muddibr@gmail.com>
2020-01-14 21:33:43 -0300

    Add project files.

---
+++ EFCoreRowVersionRepro/Entity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreRowVersionRepro
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public ulong RowVersion { get; set; }
+    }
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Integrated Security=true;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Entity>()
+                .Property(x => x.RowVersion)
+                .IsRowVersion()
+                .HasConversion<byte[]>();
+        }
+    }
+}
---
+++ EFCoreRowVersionRepro/Migrations/20200115003140_RowVersionMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreRowVersionRepro.Migrations
+{
+    public partial class RowVersionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RowVersion = table.Column<byte[]>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entities");
+        }
+    }
+}
